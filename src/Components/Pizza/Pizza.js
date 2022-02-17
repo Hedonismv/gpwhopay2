d86e190bd691d91c85d220309b177bc2df8be721
Vitaly <35634423+Hedonismv@users.noreply.github.com>
2022-02-18 01:18:59 +0300

    Add pizza title

--- src/Components/Pizza/Pizza.js
+++ src/Components/Pizza/Pizza.js
@@ -1,34 +1,38 @@
-import React, {useContext} from 'react';
-import './pizza.css';
-import {PaymentContext} from "../../App";
+import React, { useContext } from "react";
+import "./pizza.css";
+import { PaymentContext } from "../../App";
 
-const Pizza = ({guests, eaters, slices}) => {
+const Pizza = ({ guests, eaters, slices }) => {
+  const { isLoaded, pizza } = useContext(PaymentContext);
 
-	const {isLoaded} = useContext(PaymentContext)
-
-	return (
-		<div className={`content ${isLoaded ? '' : 'hide'}`} id="content_block">
-			<div className="pizza_container" id="pizza_visible">
-				<div className="pizza_content">
-					<div className="basil">
-						<div className="leaf"/>
-					</div>
-					<div className="onions"/>
-					<div className="olives"/>
-					<div className="pepperonis"/>
-					<div className="slices" id="pizza">
-						{slices.map(slice =>
-							<div key={slice.degree} className={'pizza_slice'} style={{transform: `rotate(${slice.degree}deg)`}}/>
-						)}
-					</div>
-				</div>
-			</div>
-			<div className={'content_info'}>
-				<span>Total participants: {guests.length}</span>
-				<span>Pizza lovers: {eaters.length}</span>
-			</div>
-		</div>
-	);
+  return (
+    <div className={`content ${isLoaded ? "" : "hide"}`} id="content_block">
+      <h1 className={"pizza_title"}>{pizza.name}</h1>
+      <div className="pizza_container" id="pizza_visible">
+        <div className="pizza_content">
+          <div className="basil">
+            <div className="leaf" />
+          </div>
+          <div className="onions" />
+          <div className="olives" />
+          <div className="pepperonis" />
+          <div className="slices" id="pizza">
+            {slices.map((slice) => (
+              <div
+                key={slice.degree}
+                className={"pizza_slice"}
+                style={{ transform: `rotate(${slice.degree}deg)` }}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className={"content_info"}>
+        <span>Total participants: {guests.length}</span>
+        <span>Pizza lovers: {eaters.length}</span>
+      </div>
+    </div>
+  );
 };
 
 export default Pizza;
